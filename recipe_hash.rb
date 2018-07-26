5835ff07319d08047fff92eb678e4667cde90d8c
clairespills <purpleprincess372@gmail.com>
2018-07-26 16:50:06 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -3,13 +3,12 @@ perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup"
 puts perfect_10_recipe
 
 
-def recipe_ingredients(perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder  => "1/2 tea spoon",:baking_soda => "1/4 tea spoon",:Xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"})
+def recipe_ingredients(perfect_10_recipe)
   return perfect_10_recipe
 end
 recipe_ingredients
 
-def amount_of_chocolate_chips(perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder  => "1/2 tea spoon",:baking_soda => "1/4 tea spoon",:Xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"})
-  return perfect_10_recipe[:mini_dark_chocolate_chips]
+def amount_of_chocolate_chips(perfect_10_recipe)
 end
 
 amount_of_chocolate_chips
