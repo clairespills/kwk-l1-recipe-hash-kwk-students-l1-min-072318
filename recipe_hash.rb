0c7c34c920945f8c1758e85f2e1d6ac5c07ebf24
clairespills <purpleprincess372@gmail.com>
2018-07-26 16:47:43 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -15,7 +15,7 @@ end
 amount_of_chocolate_chips
 
 
-def ingredients_and_amounts(perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder  => "1/2 tea spoon",:baking_soda => "1/4 tea spoon",:Xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"})
+def ingredients_and_amounts(perfect_10_recipe)
   perfect_10_recipe.each do |x,y|
     return "#{x} #{y}"
   end
