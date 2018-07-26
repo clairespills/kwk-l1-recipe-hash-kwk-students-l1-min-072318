6ee6927f070baac9d612f590d081383ba7e640aa
clairespills <purpleprincess372@gmail.com>
2018-07-26 16:43:40 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -36,5 +36,6 @@ def amounts(perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oat
 end
 
 
-def number_ingredients()
+def number_ingredients(perfect_10_recipe={:almond_flour => "2 cups", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder  => "1/2 tea spoon",:baking_soda => "1/4 tea spoon",:Xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"})
+  
 end
